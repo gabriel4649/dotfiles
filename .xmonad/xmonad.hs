f64e7f97bb9c3be0e616ae148bc46dc019cf408d
Gabriel Perez <gabrieljoel@gmail.com>
2012-01-26 11:10:45 -0400

    I prefer tiled chat. Changed workspaces.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,11 +8,12 @@ myManageHook = composeAll (
     , className =? "Unity-2d-panel" --> doIgnore
     , className =? "Unity-2d-launcher" --> doIgnore
     , className =? "Gimp" --> doFloat
-    , className =? "Empathy" --> doFloat  
+    , className =? "Empathy" --> doShift "3:chat"
+    , className =? "Emacs" --> doShift "2:emacs"
     , className =? "Firefox" --> doShift "1:web"
     ])
 
-myWorkspaces = ["1:web","2:emacs","3","4:docs","5","6","7","8","9"]  
+myWorkspaces = ["1:web","2:emacs","3:chat","4:docs","5","6","7","8","9"]  
 
 
 main = xmonad gnomeConfig 
