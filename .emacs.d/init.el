13079e3fbf6981224dfddecf4b87d34b93b20f0f
Gabriel Perez <gabrieljoel@gmail.com>
2012-10-06 20:50:24 -0400

    Custom set variables changed due to theme change

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -246,14 +246,15 @@
 (wg-load "~/.emacs.d/workgroups")
 
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("21d9280256d9d3cf79cbcf62c3e7f3f243209e6251b215aede5026e0c5ad853f" default)))
  '(org-agenda-files (quote ("~/Ubuntu One/org/algundia.org" "~/Ubuntu One/org/capture.org" "~/Ubuntu One/org/diario.org" "~/Ubuntu One/org/flagged.org" "~/Ubuntu One/org/habitos.org" "~/Ubuntu One/org/libreta.org" "~/Ubuntu One/org/migtd.org" "~/Ubuntu One/org/recordatorios.org"))))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
