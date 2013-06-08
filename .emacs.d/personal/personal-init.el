a6901469d1d99b2cdba2ec0cef6c8333192079bc
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-06-08 17:26:21 -0400

    Swank-js is working now.

--- .emacs.d/personal/personal-init.el
+++ .emacs.d/personal/personal-init.el
@@ -88,6 +88,27 @@
 (add-hook 'web-mode-hook (lambda () (whitespace-mode -1)))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
+;; js2 mode
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+;; Slime js
+(add-to-list 'load-path "~/.npm/swank-js/0.0.5/package/")
+(slime-setup '(slime-js slime-repl))
+(setq slime-js-swank-command "/usr/local/bin/swank-js")
+(setq slime-js-swank-args '())
+
+(global-set-key [f5] 'slime-js-reload)
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (slime-js-minor-mode 1)))
+
+;; For slime js
+(add-hook 'css-mode-hook
+          (lambda ()
+            (define-key css-mode-map "\M-\C-x" 'slime-js-refresh-css)
+            (define-key css-mode-map "\C-c\C-r" 'slime-js-embed-css)))
+
 ;; Arduino stuff
 (setq auto-mode-alist (cons '("\\.\\(pde\\|ino\\)$" . arduino-mode) auto-mode-alist))
 (autoload 'arduino-mode "arduino-mode" "Arduino editing mode." t)
