05819810a583db4792920a8124aedef04535b38c
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-07-13 11:49:14 -0400

    Removes line numbers.

--- .emacs.d/personal/appearance.el
+++ .emacs.d/personal/appearance.el
@@ -2,12 +2,6 @@
 ; http://emacs-fu.blogspot.com/2009/12/scrolling.html
 (set-scroll-bar-mode nil)
 
-; Enable line number display
-(global-linum-mode t)
-
-;; use soothe as the default theme
-;(require 'soothe-theme)
-
 ;; Use Solarized light as the default theme
 (load-theme 'solarized-light t)
 
