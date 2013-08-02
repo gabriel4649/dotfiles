bae0b7860b2fe145d0643667621b6b1bb8003c26
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-08-02 15:50:31 -0400

    Remove dafault theme.

--- .emacs.d/personal/setup-org-mode.el
+++ .emacs.d/personal/setup-org-mode.el
@@ -1,5 +1,5 @@
 ;; Take care of extra dependencies
-(prelude-ensure-module-deps '(org org-bullets org-octopress))
+(prelude-ensure-module-deps '(org org-bullets org-octopress audio-notes-mode))
 
 ;; Set to the location of your Org files on your local system
 (setq org-directory "~/Ubuntu One/org")
