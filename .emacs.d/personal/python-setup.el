fc095adea08bb5eb35377d37575b4960b8cd8d92
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-08-30 14:28:36 -0400

    Adds new python setup file.

---
+++ .emacs.d/personal/python-setup.el
@@ -0,0 +1,6 @@
+(require 'virtualenvwrapper)
+(venv-initialize-interactive-shells) ;; if you want interactive shell support
+(venv-initialize-eshell) ;; if you want eshell support
+(setq venv-location "~/.virtualenvs")
+
+(provide 'python-setup)
