7ea5c31901ddb1e73bcdbd61f9ac2b78a5001b90
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2014-04-19 13:06:18 -0400

    Adds sublimity for smooth scrolling.

--- .emacs.d/personal/appearance.el
+++ .emacs.d/personal/appearance.el
@@ -1,13 +1,19 @@
 ;; Take care of extra dependencies
-(prelude-ensure-module-deps '(solarized-theme))
+(prelude-ensure-module-deps '(solarized-theme sublimity))
 
-; Remove scrollbar
+;  Remove scrollbar
 ; http://emacs-fu.blogspot.com/2009/12/scrolling.html
 (set-scroll-bar-mode nil)
 
 ;; Use Solarized light as the default theme
 ;; (load-theme 'solarized-light t)
-;; Use Solarized light as the default theme
+;; Use Solarized dark as the default theme
 (load-theme 'solarized-dark t)
+
+(require 'sublimity)
+(require 'sublimity-scroll)
+;; (require 'sublimity-map)
+(require 'sublimity-attractive)
+(sublimity-mode 1)
 
 (provide 'appearance)
