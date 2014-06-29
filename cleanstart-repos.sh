3813085d92647e2cad5f99d96286869debb5d0f8
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2014-06-29 19:12:31 -0400

    Adds missing repos.

--- cleanstart-repos.sh
+++ cleanstart-repos.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo add-apt-repository ppa:ubuntu-elisp/ppa
-sudo apt-add-repository ppa:caffeine-developers/ppa
+sudo add-apt-repository ppa:caffeine-developers/ppa
+sudo add-apt-repository ppa:fossfreedom/byzanz
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
