b5790d55682d1c962306e6a24c67dfe40262f6d3
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2014-10-20 13:36:17 -0400

    Adds weather to Emacs.

---
+++ .emacs.d/personal/weather.el
@@ -0,0 +1,6 @@
+;; Take care of extra dependencies
+(prelude-ensure-module-deps '(weather-metno))
+
+(setq weather-metno-location-name "Atlanta, GA"
+      weather-metno-location-latitude 33.862100
+      weather-metno-location-longitude -84.687900)
