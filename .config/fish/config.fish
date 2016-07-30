e4c163a4c254a41291f202dcbb1db5f5d954c4c7
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2016-07-30 17:29:25 -0400

    Cleansup fish config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,40 +1,19 @@
 set PATH ~/.scripts/ $PATH
 
-set PATH ~/frameworks/ $PATH
-
-# Make Android stuff available
-set PATH ~/frameworks/android-sdk-linux/tools $PATH
-set PATH ~/frameworks/android-sdk-linux/platform-tools $PATH
-
-function resume
-     ssh climatewarrior_gabrieljperez@ssh.phx.nearlyfreespeech.net 
-end
-
-function curioso_blog
-     ssh climatewarrior_curioso@ssh.phx.nearlyfreespeech.net 
-end
-
-function curioso_shop
-     ssh climatewarrior_curioso-shop@ssh.phx.nearlyfreespeech.net 
-end
-
-function cuda
-     ssh climatewarrior@136.145.116.227	
-end
-
 function emacs
 	emacsclient -t $argv
 end 
 
 set EDITOR emacsclient -t
 
-# Source virtualfish for virtualenvs
+# virtualfish for virtualenvs
 # https://github.com/adambrenecki/virtualfish
-set VIRTUALFISH_COMPAT_ALIASES 1
-. ~/.scripts/virtualfish/virtual.fish
+eval (python -m virtualfish)
 
 # Fix for fish in emacs
 # https://github.com/fish-shell/fish-shell/issues/107
 function fish_title
   true
 end
+
+alias sf "symfony"
