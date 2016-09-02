e3ed8bf3c4a884758f0b9db7880af7496266c5ee
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2016-09-02 18:43:44 -0400

    Improves keyboard setup script

--- keyboard-setup.sh
+++ keyboard-setup.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 # Use caps lock as extra ctrl
+
+# For Unity
 dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:nocaps']"
+
+# For LXDE
+setxkbmap -layout "$(setxkbmap -print | awk -F + '/xkb_symbols/ {print $2}')" -option ctrl:nocaps
