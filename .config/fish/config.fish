9fe813535117048c12232227a1affc73cc5bc0e9
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2016-09-24 17:15:32 -0400

    Simplify fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,3 @@
-set PATH ~/.scripts/ $PATH
-
 function emacs
 	emacsclient -t $argv
 end 
@@ -9,11 +7,3 @@ set EDITOR emacsclient -t
 # virtualfish for virtualenvs
 # https://github.com/adambrenecki/virtualfish
 eval (python -m virtualfish)
-
-# Fix for fish in emacs
-# https://github.com/fish-shell/fish-shell/issues/107
-function fish_title
-  true
-end
-
-alias sf "symfony"
