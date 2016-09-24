4e629794da5dde13b34ed886ef39d7c1612a38d5
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2016-09-24 16:56:55 -0400

    Simplifies emacs configuration

--- .emacs.d/personal/appearance.el
+++ .emacs.d/personal/appearance.el
@@ -1,22 +1,18 @@
 ;; Take care of extra dependencies
-(prelude-ensure-module-deps '(solarized-theme sublimity))
+(prelude-ensure-module-deps '(solarized-theme nyan-mode sublimity))
 
-;  Remove scrollbars
-(scroll-bar-mode -1)
-(horizontal-scroll-bar-mode -1)
-
-;; Use Solarized light as the default theme
-;; (load-theme 'solarized-light t)
 ;; Use Solarized dark as the default theme
 (load-theme 'solarized-dark t)
 
+;; Adds nyan scroll
 (require 'nyan-mode)
 (nyan-mode)
 
 (require 'sublimity)
-(require 'sublimity-scroll)
+(require 'sublimity-scroll) ;; smooth scrolling
+(require 'sublimity-attractive) ;; distraction free mode
+(sublimity-attractive-hide-bars) ;; hide scroll bars
 ;; (require 'sublimity-map)
-(require 'sublimity-attractive)
 (sublimity-mode 1)
 
 (provide 'appearance)
--- .emacs.d/personal/personal-init.el
+++ .emacs.d/personal/personal-init.el
@@ -1,31 +1,5 @@
 ;; Take care of extra dependencies
-(prelude-ensure-module-deps '(auto-complete multiple-cursors
-                                            zencoding-mode
-                                            discover-my-major))
-
-;;; Add extra emacs modules to load path
-;;;; http://stackoverflow.com/questions/221365/emacs-lisp-how-to-add-a-folder-and-all-its-first-level-sub-folders-to-the-load
-(let ((base "~/.emacs.d/personal/elisp"))
-  (add-to-list 'load-path base)
-  (dolist (f (directory-files base))
-    (let ((name (concat base "/" f)))
-      (when (and (file-directory-p name)
-                 (not (equal f ".."))
-                 (not (equal f ".")))
-        (add-to-list 'load-path name)))))
-
-;; Add key-binding for auto-fill-mode
-;; http://emacswiki.org/emacs/AutoFillMode
-(global-set-key (kbd "C-c q") 'auto-fill-mode)
-; Turn on auto-fill automatically for org-mode files
-(add-hook 'org-mode-hook 'turn-on-auto-fill)
-
-;; Add key-binding for discover-my-major
-(global-set-key (kbd "C-h C-m") 'discover-my-major)
-
-;; Global auto-complete
-(require 'auto-complete)
-(global-auto-complete-mode t)
+(prelude-ensure-module-deps '(auto-complete multiple-cursors))
 
 ; Activate Multiple Cursors
 ; https://github.com/magnars/multiple-cursors.el
@@ -35,18 +9,5 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-
-;; Arduino stuff
-(setq auto-mode-alist (cons '("\\.\\(pde\\|ino\\)$" . arduino-mode) auto-mode-alist))
-(autoload 'arduino-mode "arduino-mode" "Arduino editing mode." t)
-
-; Activate zencoding
-; https://github.com/rooney/zencoding
-(require 'zencoding-mode)
-;(add-hook 'sgml-mode-hook 'zencoding-mode) ;; Auto-start on any markup modes
-
-; Ediff setup split windows
-(setq ediff-split-window-function 'split-window-horizontally
-      ediff-window-setup-function 'ediff-setup-windows-plain)
 
 (provide 'personal-init)
