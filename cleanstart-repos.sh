863251e030b5335537c7d771253b133f5381de4e
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2016-09-24 17:06:34 -0400

    Removes ubuntu tweak repository

--- cleanstart-repos.sh
+++ cleanstart-repos.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo add-apt-repository ppa:ubuntu-elisp/ppa # Emacs
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh # Heroku
-sudo add-apt-repository ppa:tualatrix/ppa # Ubuntu Tweak
