64859c7d74d974dce1134a6cd043474fb340d477
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2016-09-28 09:36:05 -0400

    Switched to spacemacs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,3 @@
-function emacs
-	emacsclient -t $argv
-end
-
-function emacs-restart
-         killall emacs
-         /usr/bin/emacs --daemon
-end 
-
-set EDITOR emacsclient -t
-
 # virtualfish for virtualenvs
 # https://github.com/adambrenecki/virtualfish
 eval (python -m virtualfish)
--- .emacs.d/personal/python-setup.el
+++
@@ -1,9 +0,0 @@
-;; Take care of extra dependencies
-(prelude-ensure-module-deps '(virtualenvwrapper))
-
-(require 'virtualenvwrapper)
-(venv-initialize-interactive-shells) ;; if you want interactive shell support
-(venv-initialize-eshell) ;; if you want eshell support
-(setq venv-location "~/.virtualenvs")
-
-(provide 'python-setup)
