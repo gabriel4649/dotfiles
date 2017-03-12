8a3e835479cd20b37b62603ba6a2c73a546f8301
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2017-03-12 10:01:13 -0400

    Sets yarn path for globals

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
 # virtualfish for virtualenvs
 # https://github.com/adambrenecki/virtualfish
 eval (python -m virtualfish)
+
+# Yarn global binaries
+set -U fish_user_paths ~/.yarn-global/bin $fish_user_paths
