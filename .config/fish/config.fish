3081146d96394305177e70010951e8bfe7b1cf99
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2018-09-18 09:53:45 -0400

    Adds path for yarn global binaries

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,4 @@ eval (python -m virtualfish)
 
 # Yarn global binaries
 set -U fish_user_paths ~/.config/yarn/bin $fish_user_paths
+set -U fish_user_paths ~/.config/yarn/global/node_modules/.bin $fish_user_paths
